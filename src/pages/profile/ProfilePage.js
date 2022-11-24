e761ef33f781703306d4fb4719009967a692f75b
Ivana-Lida Heintze <lydiaive@gmail.com>
2022-11-24 21:04:13 +0100

    profile container layouts

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import CurveContainerLeft from '../../components/common/CurveContainerLeft'
+import CurveContainerRight from '../../components/common/CurveContainerRight'
 import NavMenue from '../../components/navigation/NavMenue'
+import ProfileHeader from '../../components/profile/ProfileHeader'
+import EditProfileHeader from '../../components/profile/ProfileHeader'
+
 
 export default function ProfilePage() {
    return (
     <div className="page-relative">
-      <NavMenue></NavMenue>  
-      <main className="main-with-nav">
-         <h1>Profile</h1>
-         <section className="test-section">test</section>
-         <section className="test-section">test</section>
-         <section className="test-section">test</section>
-         <section className="test-section">test</section>
-         <section className="test-section">test</section>
-      </main>
+        <NavMenue></NavMenue> 
+        <ProfileHeader 
+            profileHeadline="@Username" 
+            profileSubheadline="Location"
+            /> 
+        {/* <EditProfileHeader 
+        profileHeadline="Welcome" 
+        profileSubheadline="Upload Image"
+        />  */}
+        <main>
+            <CurveContainerLeft></CurveContainerLeft>
+            <CurveContainerRight></CurveContainerRight>
+        </main>
     </div>
    )
 }
