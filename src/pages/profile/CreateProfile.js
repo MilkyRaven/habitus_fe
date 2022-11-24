8845bd4c0f6ac252ecbcb2402c41c7bfbebee75e
Vicky Ricart <vickyc.ricart@gmail.com>
2022-11-24 16:55:25 +0100

    SignUp, Login, Create Profile Created

---
+++ src/pages/profile/CreateProfile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CreateProfileForm from '../../components/profile/CreateProfileForm';
+
+
+export default function CreateProfile() {
+
+    return (
+     <div className="page-relative">
+        <h1>Create Profile</h1>     {/*Can delete this line */}
+        <h1>Welcome</h1>
+        <CreateProfileForm />
+    </div>
+
+    )
+ }
