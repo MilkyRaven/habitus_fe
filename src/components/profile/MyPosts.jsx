237a0e68db6d2524342d3e0d275de9e41daa5846
Milky Raven <heartonllamas@gmail.com>
2022-11-25 17:47:55 +0100

    creating component myPosts

---
+++ src/components/profile/MyPosts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const apiEndpoint = "http://localhost:8000/api/my-profile/my-posts";
+
+export default function MyPosts() {
+
+  return (
+    <div>MyPosts</div>
+  )
+}
