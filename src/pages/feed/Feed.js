1c6000fb1155452be9daae0a46888ebce3b9f80a
Milky Raven <heartonllamas@gmail.com>
2022-11-27 20:11:23 +0100

    working on LibraryPage and MyPosts

--- src/pages/feed/Feed.js
+++ src/pages/feed/Feed.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavMenue from '../../components/navigation/NavMenue'
+import MyPosts from '../../components/profile/MyPosts'
 
 export default function Feed () {
 
     return (
         <div>
             <h1>My Feed</h1> {/* Can delete later */}
+            <MyPosts></MyPosts>
             <NavMenue></NavMenue> 
 
         </div>
--- src/pages/profile/LibraryPage.js
+++ src/pages/profile/LibraryPage.js
@@ -1,8 +1,37 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
+const apiEndpoint = "http://localhost:8000/api/my-profile/library";
 
 export default function LibraryPage () {
 
+    const [library, setLibrary] = useState([]);
+    
+    useEffect(() => {
+        const apiCall = async () => {
+           const res = await axios.get(apiEndpoint)
+           setLibrary(res.data)
+        }
+  
+        apiCall()
+     }, [])
+
+    //  console.log(library)
+
     return(
-        <h1>This is the Library</h1>
+        <div>
+            <h1>This is my Library</h1>
+                {library.forEach((post)=> {
+                   return (
+                    <div> 
+                        <p>{post.creator}</p>
+                        <p>{post.title}</p>
+                        <p>{post.image}</p>
+                        <p>{post.createdAt}</p>
+                        <button>Delete from library</button>
+                    </div>
+                   )
+                })}
+        </div>
     )
 }
