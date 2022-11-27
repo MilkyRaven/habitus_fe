7786881a2387f49428fa3e91ddf98d5bce546e44
Milky Raven <heartonllamas@gmail.com>
2022-11-27 20:45:51 +0100

    added route to chat on nav-menue

--- src/components/navigation/NavMenue.js
+++ src/components/navigation/NavMenue.js
@@ -14,7 +14,7 @@ export default function NavMenue() {
       <nav id="nav-menue">
         <NavLink to="/feed" className="menue-icon-container"><img className="menue-icon" src={homePlacholder} alt="feed"/></NavLink>
         <NavLink to="/profile" className="menue-icon-container"><img className="menue-icon" src={homePlacholder} alt="profile"/></NavLink>
-        <NavLink className="menue-icon-container"><img className="menue-icon" src={homePlacholder} alt="meetings"/></NavLink>
+        <NavLink to="/chat"className="menue-icon-container"><img className="menue-icon" src={homePlacholder} alt="chat"/></NavLink>
         <NavLink className="menue-icon-container"><img className="menue-icon" src={homePlacholder} alt="chats"/></NavLink>
         {isLoggedIn && <button onClick={() => logOutUser()}>Log out</button>}
          <div className="curved corner-b-left b-left-grey"></div>
