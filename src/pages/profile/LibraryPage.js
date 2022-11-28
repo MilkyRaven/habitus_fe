af5b4495a48c97355f1469304edf65383eba79a7
Milky Raven <heartonllamas@gmail.com>
2022-11-28 16:44:52 +0100

    edited library page

--- src/pages/profile/LibraryPage.js
+++ src/pages/profile/LibraryPage.js
@@ -1,37 +1,38 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const apiEndpoint = "http://localhost:8000/api/my-profile/library";
 
-export default function LibraryPage () {
+export default function LibraryPage() {
 
     const [library, setLibrary] = useState([]);
-    
+
     useEffect(() => {
         const apiCall = async () => {
-           const res = await axios.get(apiEndpoint)
-           setLibrary(res.data)
+            const token = localStorage.getItem("authToken");
+            const res = await axios.get(apiEndpoint, { headers: { Authorization: `Bearer ${token}` }})
+            setLibrary(res.data)
         }
-  
+
         apiCall()
-     }, [])
+    }, [])
 
-    //  console.log(library)
+    console.log(library)
 
-    return(
+    return (
         <div>
             <h1>This is my Library</h1>
-                {library.forEach((post)=> {
-                   return (
-                    <div> 
-                        <p>{post.creator}</p>
+            {library.map((post) => {
+                return (
+                    <div key={post._id}>
+                        <p>{post.creator.name}</p>
                         <p>{post.title}</p>
-                        <p>{post.image}</p>
+                        <img width={200} alt="post" src={post.image}></img>
                         <p>{post.createdAt}</p>
                         <button>Delete from library</button>
                     </div>
-                   )
-                })}
+                )
+            })}
         </div>
     )
 }
