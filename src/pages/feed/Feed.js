d9bc8534ef4dd918c28e602cb2e777950089718a
Vicky Ricart <vickyc.ricart@gmail.com>
2022-11-29 18:10:03 +0100

    Feed CSS Edited - Modal Added

--- src/pages/feed/Feed.js
+++ src/pages/feed/Feed.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavMenue from '../../components/navigation/NavMenue'
 import MyPosts from '../../components/profile/MyPosts'
 import PopularPosts from '../../components/feed/PopularPosts';
 import FriendsPosts from '../../components/feed/FriendsPosts';
 import FreshPosts from '../../components/feed/FreshPosts';
+import ModalModule from '../../components/common/ModalModule';
 import CreatePostForm from '../../components/post/CreatePostForm'
+import './feed.css'
 
 export default function Feed () {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const closeModal = () => {
+        setIsOpen(false)
+    }
 
     return (
         <div>
-            <CreatePostForm />
+            <button id="btn-create-post" onClick={() => setIsOpen(true)}>Create</button>
+            {isOpen && 
+                <ModalModule setIsOpen={setIsOpen}>
+                    <CreatePostForm closeModalHandler={closeModal}/>
+                </ModalModule>
+    
+            }
+            
             <PopularPosts />
             <FriendsPosts />
             <FreshPosts />
             <NavMenue></NavMenue>
-
         </div>
     )
 }
