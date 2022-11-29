3e1326a4ccc2d8883f62b949a48cfb6b29e3988d
Milky Raven <heartonllamas@gmail.com>
2022-11-29 12:46:50 +0100

    update

--- src/pages/feed/Feed.js
+++ src/pages/feed/Feed.js
@@ -4,15 +4,17 @@ import MyPosts from '../../components/profile/MyPosts'
 import PopularPosts from '../../components/feed/PopularPosts';
 import FriendsPosts from '../../components/feed/FriendsPosts';
 import FreshPosts from '../../components/feed/FreshPosts';
+import CreatePostForm from '../../components/post/CreatePostForm'
 
 export default function Feed () {
 
     return (
         <div>
+            <CreatePostForm />
             <PopularPosts />
             <FriendsPosts />
             <FreshPosts />
-            <NavMenue></NavMenue> 
+            <NavMenue></NavMenue>
 
         </div>
     )
