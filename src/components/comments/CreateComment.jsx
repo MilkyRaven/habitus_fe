a0dc0d5e5f305a2382d0a9c77e2a356cf67fe2d8
Milky Raven <heartonllamas@gmail.com>
2022-11-29 22:17:35 +0100

    able to comment and delete comments

---
+++ src/components/comments/CreateComment.jsx
@@ -0,0 +1,52 @@
+import axios from 'axios'
+import React, {  useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+
+export default function CreatePostForm (props) {
+    const { postId } = useParams()
+    console.log(postId) 
+    const storedToken = localStorage.getItem('authToken')
+    
+    const [select, setSelect] = useState([])
+
+    const [input, setInput] = useState({
+        content: ""
+        })
+
+    const handleChange = (event) => { 
+        setInput (prev => {
+            return {
+            ...prev,
+            [event.target.name]: event.target.value
+            }
+        })
+    }
+        
+    const submitHandler = async (event) => {
+        event.preventDefault()
+        const {content} = input
+        const obj = {
+            content: content
+        }
+        console.log(obj, "Submit")
+
+         try {
+            await axios.post(`http://localhost:8000/api/feed/${postId}/new-comment`, obj, {headers: {Authorization: `Bearer ${storedToken}`}})
+
+        } catch (err) {
+            console.log(err)
+        } 
+    }
+
+    return (
+        <form className="form" onSubmit={submitHandler}>
+            <div className="form-row">
+                <label>Leave your comment here</label>
+                <textarea name="content" rows="4" cols="39" value={input.content} onChange={handleChange}></textarea>            
+            </div>
+            <button type='submit'>Comment</button>
+        </form>
+    )
+    
+}
