cd93fdb42e6f8cd84cfeeb8d105fc690a09e2694
Milky Raven <heartonllamas@gmail.com>
2022-12-01 11:56:43 +0100

    following and followers components

---
+++ src/components/users/Following.jsx
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+
+const apiEndpoint = "http://localhost:8000/api/user/following"
+
+export default function Following() {
+
+    const [following, setFollowing] = useState([]);
+
+    useEffect(() => {
+        const apiCall = async () => {
+            const token = localStorage.getItem("authToken");
+            try {
+                const res = await axios.get(apiEndpoint, { headers: { Authorization: `Bearer ${token}` }});
+                setFollowing(res.data.following)
+                console.log(res.data.following);
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        apiCall();
+    }, [])
+
+    console.log(following)
+
+  return (
+    <div>Following
+
+        <div>
+        {following[0] ? following.map((user)=> {
+                return (<div key={user._id}>
+                <p><Link to={`/user/${user._id}`}>{user.username}</Link></p>
+                <img alt='user profile' width={50} src={user.profileImg}></img>
+                </div>)
+            }) : <p>You are not following anyone yet</p> }
+        </div>
+    </div>
+  )
+}
